5811a9e4b28367cb2477ecd0db02c65fff9fd1f6
baenisam <baenisam@gmail.com>
2023-09-01 15:38:21 +0200

    edit document file

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,13 +9,13 @@ export default class MyDocument extends Document {
                 <Head>
                     <base href={ process.env.PUBLIC_URL } />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7CPoppins:300,400,500,600,700" />
-                    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-                    <link rel="stylesheet" type="text/css" href="css/fonts-molla.min.css" />
-                    <link rel="stylesheet" type="text/css" href="vendor/line-awesome/css/line-awesome.min.css" />
+                    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
+                    <link rel="stylesheet" type="text/css" href="/css/fonts-molla.min.css" />
+                    <link rel="stylesheet" type="text/css" href="/vendor/line-awesome/css/line-awesome.min.css" />
                 </Head>
                 <body>
                     <Main />
-                    <script src="js/jquery.min.js"></script>
+                    <script src="/js/jquery.min.js"></script>
                     <NextScript />
                 </body>
             </Html>
